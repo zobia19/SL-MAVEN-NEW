2a203c6b2a43428f67dbb7f86c0140083370d513
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-11-30 10:04:34 +0530

    Sum() Added

---
+++ src/main/java/devops/sl/MyCalc.java
@@ -0,0 +1,20 @@
+package devops.sl;
+
+public class MyCalc {
+	
+	int sum(int a, int b)
+	{
+		return a+b;
+	}
+	int diff(int a, int b)
+	{
+		return a-b;
+	}
+
+	public static void main(String[] args) {
+
+		MyCalc calc = new MyCalc();
+		System.out.println("Sum is : "+calc.sum(20, 10));
+		System.out.println("Diff is : "+calc.diff(20, 10));
+	}
+}
